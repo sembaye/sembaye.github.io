07d1d20093d7c12619acb7fb1fdf0cfd56083344
sembaye <sokrates.embaye@gmail.com>
2017-03-31 15:02:02 +0200

    update repo loading

---
+++ orgs.js
@@ -0,0 +1,18 @@
+
+    var orgs = [
+        /*
+		{"name": "Repo_User/Repo_name",
+         "type": "repo"},
+        {"name": "User_of_Pagename",
+          "type": "org",
+          "link": "http://Pagename.github.io/"},
+        {"name": "Type in User Name",
+          "type":"user"},
+		  */
+		{"name": "Hilscher/netIOT-apps-netiotPN",
+         "type": "repo"},
+		 {"name": "Hilscher/netIOT-starterkit-azure-app",
+         "type": "repo"},
+		 {"name": "Hilscher/node-red-contrib-s7comm",
+         "type": "repo"},
+    ];
